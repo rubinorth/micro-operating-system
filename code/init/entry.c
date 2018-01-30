d14886d4e16cf431ae96e6fbbcc779d7fc11edbb
rubinorth <1134591497@qq.com>
2018-01-30 10:44:08 +0800

    add gdt

--- code/init/entry.c
+++ code/init/entry.c
@@ -3,15 +3,17 @@
  内核的入口函数
  */
 
+#include "gdt.h"
 #include "console.h"
 #include "debug.h"
 
 int kern_entry()
 {
 	init_debug();
+	init_gdt();
+	
 	console_clear();
 	printk_color(rc_black, rc_green, "Hello, my kernel!!!\n");
-	panic("test");
 	
 	return 0;
 }
