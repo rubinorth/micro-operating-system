c8b3d8bad7bb00b40751b81d180a6885c047e37f
rubinorth <1134591497@qq.com>
2018-01-30 11:27:18 +0800

    添加中断机制

--- code/init/entry.c
+++ code/init/entry.c
@@ -6,15 +6,19 @@
 #include "gdt.h"
 #include "console.h"
 #include "debug.h"
+#include "idt.h"
 
 int kern_entry()
 {
 	init_debug();
 	init_gdt();
+	init_idt();
 	
 	console_clear();
 	printk_color(rc_black, rc_green, "Hello, my kernel!!!\n");
 	
+	asm volatile("int $0x3");
+	asm volatile("int $0x4");
 	return 0;
 }
 
