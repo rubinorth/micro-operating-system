e01712a91fb81d3b35eb9282ba65f3f1fdefa24d
rubinorth <1134591497@qq.com>
2018-01-31 11:23:57 +0800

    输出物理内存信息

--- code/init/entry.c
+++ code/init/entry.c
@@ -8,6 +8,7 @@
 #include "debug.h"
 #include "idt.h"
 #include "timer.h"
+#include "pmm.h"
 
 int kern_entry()
 {
@@ -19,7 +20,12 @@ int kern_entry()
 	printk_color(rc_black, rc_green, "Hello, my kernel!!!\n");
 	
 	init_timer(200);
-	asm volatile("sti");
+	
+	printk("kernel in memory start: 0x%08X\n", kern_start);
+	printk("kernel in memory end: 0x%08X\n", kern_end);
+	printk("kernel in memory used: %d KB\n\n", (kern_end - kern_start + 1023) / 1024);
+	show_memory_map();
+	
 	return 0;
 }
 
