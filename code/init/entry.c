7f1e151af9352a85ea40111773b0808e43960da6
rubinorth <1134591497@qq.com>
2018-01-31 11:33:58 +0800

    对物理内存分页，并输出前几页的物理地址

--- code/init/entry.c
+++ code/init/entry.c
@@ -26,6 +26,17 @@ int kern_entry()
 	printk("kernel in memory used: %d KB\n\n", (kern_end - kern_start + 1023) / 1024);
 	show_memory_map();
 	
+	init_pmm();
+
+	printk_color(rc_black, rc_red, "\nThe Count of Physical Memory Page is: %u\n\n", phy_page_count);
+
+	uint32_t allc_addr = NULL;
+	printk_color(rc_black, rc_light_brown , "Test Physical Memory Alloc :\n");
+	for(int i=0;i<5;i++){
+		allc_addr = pmm_alloc_page();
+		printk_color(rc_black, rc_light_brown , "Alloc Physical Addr: 0x%08X\n", allc_addr);
+	}
+	
 	return 0;
 }
 
